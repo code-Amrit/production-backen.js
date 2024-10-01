66b3c5a1ce2cf15fca4a1afae0e1ad9408bddbee
code-Amrit <amritpalsingh.webdev@gmail.com>
2024-10-01 14:27:18 +0530

    use of pipeline in mongo db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -299,7 +299,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "User details fetched successfully"))
     //authmiddleware ton aya verifyjWT
 })
 
@@ -329,7 +329,7 @@ const updateAccountDetails = asyncHandler(async(req, res)=>{
 })
 
 const updateUserAvatar = asyncHandler(async(req, res)=>{
-
+//todo also make delete avatar old 
     const avatarLocalPath = req.file?.path
 
     if (!avatarLocalPath) {
